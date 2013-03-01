3e4d1cc5ee24768877762b13d388e4d961bb7387
Zack Wheeler <913966+Tankooni@users.noreply.github.com>
2013-02-28 22:49:09 -0500

    BOING CUBES.

--- PaintDudez/Assets/Scripts/PaintShooter.cs
+++ PaintDudez/Assets/Scripts/PaintShooter.cs
@@ -6,11 +6,13 @@ public class PaintShooter : MonoBehaviour
 	//List<PhysicMaterial> physicsMats = new List<PhysicMaterial>();
 	public PhysicMaterial [] physicsMats;
 	public int currentMat = 0;
+	Camera cam;
 	
 	// Use this for initialization
 	void Start()
 	{
 		//physicsMats = Resources.LoadAll("PhysMats") as PhysicMaterial;
+		cam = GetComponentInChildren<Camera>() as Camera;
 	}
 	
 	
@@ -38,15 +40,20 @@ public class PaintShooter : MonoBehaviour
 	
 	void ShootPaint()
 	{
-		Vector3 dir = transform.TransformDirection(Vector3.forward);
+		Vector3 dir = cam.transform.forward;
+		
 		RaycastHit hit;
 		
 		
 		if(Physics.Raycast(transform.position, dir, out hit, 10))
 		{
 			Debug.Log("Hit: " + hit.rigidbody.name);
+			Debug.Log(cam.transform.position);
 			hit.rigidbody.renderer.material.color = new Color(Random.value, Random.value, Random.value);
 			Debug.DrawLine(transform.position, hit.transform.position, Color.red, 10);
+			
+			hit.rigidbody.collider.material = physicsMats[0];
+			hit.rigidbody.AddForce(new Vector3(0,500,0));
 		}
 	}
 }
