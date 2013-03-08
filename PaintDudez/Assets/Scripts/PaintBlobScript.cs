90e56e121b37883d85b580757b12066011191a4c
Tankooni <Tankooni@localhost>
2013-03-07 22:26:40 -0500

    Throws balls that spawn one decal.

---
+++ PaintDudez/Assets/Scripts/PaintBlobScript.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class PaintBlobScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	void OnCollisionEnter(Collision col)
+	{
+		collider.transform.localScale *= 20f;
+		
+	}
+	
+	void OnCollisionStay(Collision col)
+	{
+		foreach (ContactPoint contact in col.contacts)
+		{
+            Debug.DrawRay(contact.point, contact.normal, Color.white, 1000000);
+			GameObject decal = Instantiate(PaintShooter.splatter, contact.point + (contact.normal * 0.001f), Quaternion.FromToRotation(Vector3.up, contact.normal)) as GameObject;
+			decal.transform.localScale = new Vector3(Random.Range(0.7f, 2.0f), Random.Range(0.7f, 2.0f), 1);
+			decal.transform.parent = contact.otherCollider.rigidbody.transform;
+			decal.renderer.material.color = Color.blue;
+        }
+
+		Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
--- PaintDudez/Assets/Scripts/PaintShooter.cs
+++ PaintDudez/Assets/Scripts/PaintShooter.cs
@@ -7,15 +7,16 @@ public class PaintShooter : MonoBehaviour
 	public PhysicMaterial [] physicsMats;
 	public int currentMat = 0;
 	Camera cam;
-	GameObject splatter;
+	public static GameObject splatter;
+	GameObject blob;
 	
 	// Use this for initialization
 	void Start()
 	{
 		//physicsMats = Resources.LoadAll("PhysMats") as PhysicMaterial;
 		cam = GetComponentInChildren<Camera>() as Camera;
-		splatter = Resources.Load("Prefabs/splatterDecal") as GameObject;
-		Debug.Log(splatter);
+		PaintShooter.splatter = Resources.Load("Prefabs/splatterDecal") as GameObject;
+		blob = Resources.Load("Prefabs/paintBlob") as GameObject;
 	}
 	
 	
@@ -30,7 +31,6 @@ public class PaintShooter : MonoBehaviour
 		
 		if(Input.GetMouseButtonDown(0))
 		{
-			Debug.Log("Fire");
 			ShootPaint();
 		}
 		
@@ -41,32 +41,31 @@ public class PaintShooter : MonoBehaviour
 		
 	}
 	
-	void OnCollsionEnter(Collision col)
-	{
-		Debug.Log("sss");
-	}
-	
 	void ShootPaint()
 	{
 		Vector3 dir = cam.transform.forward;
 		
-		RaycastHit hit;
-		
-		
-		if(Physics.Raycast(transform.position, dir, out hit, 1000))
-		{
-			GameObject decal;
-//			Debug.Log("Hit: " + hit.rigidbody.name);
-//			Debug.Log(cam.transform.position);
-			Debug.DrawLine(transform.position, hit.point, Color.red, 10);
-			
-			decal = Instantiate(splatter, hit.point + (hit.normal * 0.001f), Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject;
-			decal.transform.localScale = new Vector3(Random.Range(0.7f, 2.0f), Random.Range(0.7f, 2.0f), 1);
-			decal.transform.parent = hit.rigidbody.gameObject.transform;
-			decal.renderer.material.color = Color.blue;
-			
-			hit.rigidbody.collider.material = physicsMats[1];
-			hit.rigidbody.AddForce(new Vector3(0,500,0));
-		}
+		GameObject paint = Instantiate(blob, transform.position + transform.TransformDirection(Vector3.forward), Quaternion.identity) as GameObject;
+		paint.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward)*700);
+
+		Debug.Log(cam.transform.forward);
+//		RaycastHit hit;
+//		
+//		
+//		if(Physics.Raycast(transform.position, dir, out hit, 1000))
+//		{
+//			GameObject decal;
+////			Debug.Log("Hit: " + hit.rigidbody.name);
+////			Debug.Log(cam.transform.position);
+//			Debug.DrawLine(transform.position, hit.point, Color.red, 10);
+//			
+//			decal = Instantiate(splatter, hit.point + (hit.normal * 0.001f), Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject;
+//			decal.transform.localScale = new Vector3(Random.Range(0.7f, 2.0f), Random.Range(0.7f, 2.0f), 1);
+//			decal.transform.parent = hit.rigidbody.gameObject.transform;
+//			decal.renderer.material.color = Color.blue;
+//			
+//			hit.rigidbody.collider.material = physicsMats[1];
+//			hit.rigidbody.AddForce(new Vector3(0,500,0));
+//		}
 	}
 }
