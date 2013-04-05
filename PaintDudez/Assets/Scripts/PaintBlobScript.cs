7b3d22929926f89788b2206ebe81bb5210bc660e
l231 <l231@L231-08-145765.conc.students.local>
2013-04-04 23:00:33 -0400

    Fixed the bug where decals weren't being placed on the surface. When a collision hits, a raycast is sent towards said object from a little bit away from the collision point, and the decal is placed there. The paint also launches the player now.

--- PaintDudez/Assets/Scripts/PaintBlobScript.cs
+++ PaintDudez/Assets/Scripts/PaintBlobScript.cs
@@ -7,22 +7,49 @@ public class PaintBlobScript : MonoBehaviour
 	// Use this for initialization
 	void Start()
 	{
-	
+		
 	}
 	
 	void OnCollisionEnter(Collision col)
 	{
-		foreach (ContactPoint contact in col.contacts)
+		if(col.gameObject.tag != "Paint")
 		{
-            Debug.DrawRay(contact.point, contact.normal, Color.white, 1000000);
-			GameObject decal = Instantiate(PaintShooter.splatter, contact.point + (contact.normal * 0.001f), Quaternion.FromToRotation(Vector3.up, contact.normal)) as GameObject;
-			decal.transform.localScale = new Vector3(Random.Range(0.7f, 2.0f), Random.Range(0.7f, 2.0f), 1);
-			decal.transform.parent = contact.otherCollider.transform;
-			decal.renderer.material.color = Color.blue;
-			decal.SendMessage("SetNormal", contact.normal);
-        }
-
-		Destroy(gameObject);
+			ContactPoint contact = col.contacts[0];
+			Ray ray = new Ray(contact.point + contact.normal, -contact.normal);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 1000))
+			{
+				Debug.DrawRay(hit.point, hit.normal, Color.red, 1000);
+				Debug.Log("Stuff");
+				
+				
+				
+				GameObject decal = Instantiate(PaintShooter.splatter, hit.point + (contact.normal * 0.001f), Quaternion.FromToRotation(Vector3.up, contact.normal)) as GameObject;
+				decal.transform.localScale = new Vector3(Random.Range(0.7f, 2.0f), decal.transform.localScale.y, Random.Range(0.7f, 2.0f));
+				decal.transform.parent = contact.otherCollider.transform;
+				decal.SendMessage("SetNormal", contact.normal);
+			}
+			
+//			foreach (ContactPoint contact in col.contacts)
+//			{
+//				Ray ray = new Ray(contact.point + contact.normal, -contact.normal);
+//				//Debug.DrawRay(contact.point + contact.normal, contact.normal, Color.black, 1000);
+//				RaycastHit hit;
+//				//ray.origin = ray.GetPoint(100);
+//				
+//				
+//	            //Debug.DrawRay(contact.point, contact.normal, Color.white, 1000000);
+//				GameObject decal = Instantiate(PaintShooter.splatter, contact.point + (contact.normal * 0.001f), Quaternion.FromToRotation(Vector3.up, contact.normal)) as GameObject;
+//				decal.transform.localScale = new Vector3(Random.Range(0.7f, 2.0f), Random.Range(0.7f, 2.0f), 1);
+//				decal.transform.parent = contact.otherCollider.transform;
+//				decal.SendMessage("SetNormal", contact.normal);
+//				
+//				
+//	
+//	        }
+	
+			Destroy(gameObject);
+		}
 	}
 	
 	// Update is called once per frame
--- PaintDudez/Assets/Scripts/PaintSplotchScript.cs
+++ PaintDudez/Assets/Scripts/PaintSplotchScript.cs
@@ -5,20 +5,26 @@ public class PaintSplotchScript : MonoBehaviour
 {
 	MeshFilter mf;
 	Vector3 normal;
+	PaintSplotch paint;
+	
 	// Use this for initialization
 	void Start()
 	{
 		mf = GetComponent<MeshFilter>();
+		renderer.material.color = Color.blue;
 	}
 	
 	void OnTriggerEnter(Collider col)
 	{
-		if(col.rigidbody)
+		if(col.gameObject.name == "Player")
+		{
+			col.gameObject.SendMessage("SetVelocity", normal*30);
+		}
+		else if(col.rigidbody)
 		{
 			col.rigidbody.AddForce(normal*500);
-			Debug.DrawRay(transform.position, normal, Color.cyan, 1000);
+			//Debug.DrawRay(transform.position, normal, Color.cyan, 1000);
 		}
-		
 	}
 			
 	void OnCollisionEnter(Collision col)
@@ -35,7 +41,6 @@ public class PaintSplotchScript : MonoBehaviour
 //			Debug.DrawLine(transform.localToWorldMatrix.MultiplyPoint3x4(poin), poin+Vector3.one, Color.blue);
 //		}
 	}
-	
 	
 	void SetNormal(Vector3 vec)
 	{
