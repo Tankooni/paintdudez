7c3a859e630c3301e7a4fa3d59c79081d473650e
chjolo <chjolo@localhost>
2013-09-11 22:12:29 -0400

    Began revamping of presentation level (newPaint_Sep2013); Implemented hinge joint "hammer"; Implemented silly and broken swinger, with scripts; Temporarily disabled some narration code, and gave all paints by default;

---
+++ PaintDudez/Assets/Scripts/SwingScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingScript : MonoBehaviour {
+	
+	private bool canMove;
+	public float rotationAmount;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		canMove = true;
+		rotationAmount = 1.5f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (canMove)
+		{
+			Debug.Log(gameObject.transform.rotation);
+			var nextRotation = gameObject.transform.rotation.z + Mathf.Sin(Time.timeSinceLevelLoad * 3.0f) * rotationAmount;
+
+			gameObject.transform.Rotate(0, 0, nextRotation);
+		}
+	}
+}
